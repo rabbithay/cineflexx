58eb3e9d176f116e4296d95c3cafc415ddf6801b
coelhothay <thay.csb@gmail.com>
2021-09-05 23:44:35 -0300

    style: go back button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { TiArrowBack } from 'react-icons/ti';
 
 export default function Header() {
   return (
     <TopBar>
+      <div />
       <Link to="/">
         <h1>
           Cineflex
         </h1>
       </Link>
+      <TiArrowBack
+        color="#fff"
+        size="32px"
+      />
     </TopBar>
   );
 }
@@ -22,13 +28,14 @@ const TopBar = styled.div`
   z-index: 3;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   box-shadow: 0px 2px 4px 2px rgba(0, 0, 0, 0.1);
   top: 0px;
-
+  padding:8px;
   h1 {
     font-family: 'Zilla Slab';
     font-size: 40px;
     color: #fff;
   }
+
 `;
